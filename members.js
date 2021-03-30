7b790817e78292ff0c03b4539f97bd70cf3d325f
GoldenMC9788 <62380938+anonymous9788@users.noreply.github.com>
2021-03-30 22:14:09 +0700

    Update 1.3.2 release 2
    Change members.html database, add more members, add faq.html (WIP)

---
+++ members.js
@@ -0,0 +1,99 @@
+testFunc([
+    {
+        "sayhi":"Xin chào nha :3",
+        "name": "Owner Marshy",
+        "detailName":"Marshy",
+        "ava": "img/marshyava.png",
+        "discord": "RV | 빨간 맛#7117",
+        "job":"Owner, Werewolf Dev, Admin, Quản trò, Staff"
+    },
+    {
+        "sayhi":"Chào mọi người",
+        "name": "Werewolf Dev Krysie",
+        "detailName":"Krysie",
+        "ava": "img/kryava.png",
+        "discord": "Krysie#0907",
+        "job":"Werewolf Dev, Staff"
+    },
+    {
+        "sayhi":"Chào mọi người",
+        "name": "Werewolf Dev Khang",
+        "detailName":"Khang",
+        "ava": "img/khangava.png",
+        "discord": "Khang#9594",
+        "job":"Werewolf Dev, Admin"
+    },
+    // Edit below this line ------------------------------
+    {
+        "sayhi":"Hi bros!",
+        "name": "Werewolf Dev Hải",
+        "detailName":"Hair/Nhisme/Nhím",
+        "ava": "img/hairava.png",
+        "discord": "NHisme#4460",
+        "job":"Werewolf Dev, Admin, Side Content Moderator, Side Content Team, Quản trò, Staff"
+    },
+    {
+        "sayhi":"Chào mọi người nha!",
+        "name": "Admin KhongNoiChuyen",
+        "detailName":"Tồm bếu/Đạt",
+        "ava": "img/tomava.png",
+        "discord": "KhongNoiChuyen#0217",
+        "job":"Admin, Quản trò, Staff"
+    },
+    {
+        "sayhi":"Chào mọi người",
+        "name": "Admin Cá",
+        "detailName":"Cá/Fish",
+        "ava": "img/fishava.png",
+        "discord": "Fish#7700",
+        "job":"Admin, Con đỗ nghèo khỉ"
+    },
+    {
+        "sayhi": "It's Secrett",
+        "name": "Quản trò Secret",
+        "detailName":"Secret/Sec ngou",
+        "ava": "img/secava.png",
+        "discord": "Secret#0052",
+        "job": "Quản trò, Side Content Team, Staff"
+    },
+    {
+        "sayhi": "Chào tất cả mọi người",
+        "name": "Quản trò Hán",
+        "detailName":"Hán",
+        "ava": "img/hansava.png",
+        "discord": "Hán#0366",
+        "job": "Quản Trò, Side Content Team, Staff"
+    },
+    {
+        "sayhi": "Chào tất cả",
+        "name": "Staff Ryuk",
+        "detailName":"Ryuk",
+        "ava": "img/ryukava.png",
+        "discord": "Ryuk#1019",
+        "job": "Side Content Moderator, Side Content Team, Staff"
+    },
+    {
+        "sayhi": "2k8 sin trào các bạn",
+        "name": "Staff Taki",
+        "detailName":"Taki",
+        "ava": "img/takiava.gif",
+        "discord": "Taki#2202",
+        "job": "Side Content Team, Staff"
+    },
+    {
+        "sayhi": "Chào tất cả",
+        "name": "Staff Bbilse",
+        "detailName":"Bí tồ/Bbilse",
+        "ava": "img/biava.png",
+        "discord": "Bbilse#5965",
+        "job": "Side Content Team, Staff"
+    },
+    {
+        "sayhi": "Ohayo Mina-san",
+        "name": "DinoxTheClown",
+        "detailName":"Bí tồ/Bbilse",
+        "ava": "img/dinoxava.png",
+        "discord": "Dinox#7546",
+        "job": "Side Content Team, Staff"
+    },
+])
